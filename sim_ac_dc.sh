d920ec93576ed9e3d5e09961745c0226719da31c
Phuoc Pham <phuocphn@gmail.com>
2025-07-03 10:58:02 +0200

    break run.sh to two bash scrip files

---
+++ sim_ac_dc.sh
@@ -0,0 +1,6 @@
+rm -rf logs && mkdir logs
+
+ngspice -b -o logs/dc_analysis.log AMP_NMCF_ACDC.cir
+
+
+
--- sim_tran.sh
+++ sim_tran.sh
@@ -1,6 +1,5 @@
 rm -rf logs && mkdir logs
 
-ngspice -b -o logs/dc_analysis.log AMP_NMCF_ACDC.cir
 ngspice -b -o logs/tran_analysis.log AMP_NMCF_Tran.cir
 
 
